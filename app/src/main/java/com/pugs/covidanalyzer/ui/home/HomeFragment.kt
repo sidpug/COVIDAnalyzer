a6c33fae2dc640d54bbdc4315161ffa54c251a1e
Siddharth Pugalia <sidpug@gmail.com>
2021-06-01 19:11:03 +0530

    Initial Commit

--- app/src/main/java/com/pugs/covidanalyzer/ui/home/HomeFragment.kt
+++ app/src/main/java/com/pugs/covidanalyzer/ui/home/HomeFragment.kt
@@ -1,9 +1,12 @@
  package com.pugs.covidanalyzer.ui.home
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.pugs.covidanalyzer.R
@@ -13,6 +16,7 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
 
     private lateinit var homeViewModel: HomeViewModel
     private var _binding: FragmentHomeBinding? = null
+
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -29,7 +33,14 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-
+        view?.findViewById<Button>(R.id.button_call)?.setOnClickListener {
+            val number = "+911123978046"
+            startActivity(Intent(Intent.ACTION_DIAL, Uri.parse(number)))
+        }
+        /*view?.findViewById<Button>(R.id.button_sms)?.setOnClickListener {
+            val mail = "+911123978046"
+            startActivity(Intent(Intent.ACTION_, Uri.parse(mail)))
+        }*/
         return root
     }
 
