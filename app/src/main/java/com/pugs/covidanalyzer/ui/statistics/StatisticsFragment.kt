65077c48e1d8c1ff545636354a9b0017f9b292a9
Siddharth Pugalia <sidpug@gmail.com>
2021-08-31 19:10:00 +0530

    Minor Changes in statistics file

--- app/src/main/java/com/pugs/covidanalyzer/ui/statistics/StatisticsFragment.kt
+++ app/src/main/java/com/pugs/covidanalyzer/ui/statistics/StatisticsFragment.kt
@@ -1,11 +1,19 @@
 package com.pugs.covidanalyzer.ui.statistics
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.google.gson.JsonObject
 import com.pugs.covidanalyzer.R
+import com.pugs.covidanalyzer.Retrofit_Instance
+import com.pugs.covidanalyzer.Service
+import org.json.JSONObject
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class StatisticsFragment : Fragment(R.layout.fragment_statistics) {
 
@@ -13,13 +21,43 @@ class StatisticsFragment : Fragment(R.layout.fragment_statistics) {
         //fun newInstance() = StatisticsFragment()
     }
 
-    //private lateinit var viewModel: StatisticsViewModel
+    //private late_init var viewModel: StatisticsViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_statistics, container, false)
+        val view =  inflater.inflate(R.layout.fragment_statistics, container, false)
+        val service = Retrofit_Instance.getRetrofitInstance().create(
+            Service::class.java
+        )
+
+
+        val call: Call<JsonObject>? = service.minData
+        call?.enqueue(object : Callback<JsonObject> {
+            override fun onResponse(call: Call<JsonObject>, response: Response<JsonObject>) {
+                try {
+                    val resBody = response.body().toString()
+                    val jo = JSONObject(resBody)
+
+                    for (key in jo.keys()) {
+                        Log.e("key", key)
+                        val tot = (jo.getJSONObject(key) )
+                        for (total in tot.keys())
+                            Log.e("total", tot.keys().toString())
+                        Log.e("val",
+                            (jo.get(key)) as String
+                        )
+                    }
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+
+            override fun onFailure(call: Call<JsonObject>, t: Throwable) {}
+        })
+
+        return view
     }
 
     /*override fun onActivityCreated(savedInstanceState: Bundle?) {
